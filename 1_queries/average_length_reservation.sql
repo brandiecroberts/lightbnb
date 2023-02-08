14395718623f0e064c6d938b448754cbf1bc38fe
Brandie Roberts <brandiecroberts@gmail.com>
2023-02-08 15:34:43 -0800

    Initial queries for the lightbnb database

---
+++ 1_queries/average_length_reservation.sql
@@ -0,0 +1,3 @@
+-- AVERAGE DURATION OF ALL RESERVATIONS
+SELECT avg(end_date - start_date) as average_duration
+FROM reservations;
---
+++ 1_queries/users_login.sql
@@ -0,0 +1,4 @@
+-- DETAILS ABOUT A SINGLE USER
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
